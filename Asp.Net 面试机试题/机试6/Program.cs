eae5cb44191bdc5771f8f73b12d10d7bd0d9bfcf
haimianbao1111 <657003383@qq.com>
2018-03-09 14:38:01 +0800

    递归求和

---
+++ Asp.Net 面试机试题/机试6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 机试6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res= sum(31);
+            Console.WriteLine(res);
+            Console.WriteLine(res*12);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 递归求和1+2+...+n
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static int sum(int x)
+        {
+            if (x <= 1)
+            {
+                return x;
+            }
+            else
+            {
+                return x + sum(x - 1);
+            }
+        }
+    }
+}
